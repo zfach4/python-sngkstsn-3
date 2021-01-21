654f1cb9c7578b111fac813e1aa42d42ff439e90
zfach4 <zfach12@gmail.com>
2021-01-21 18:05:32 +0700

    latihan1 hari kamis

--- latihan1.py
+++ latihan1.py
@@ -1,1 +1,22 @@
-print('hahaha')
+myList = []
+
+myList.append('zzz')
+myList.append('yyy')
+myList.append('xxx')
+
+myList2 = []
+
+myList2.append('aaa')
+myList2.append('bbb')
+myList2.append('ccc')
+
+myListAll = myList + myList2
+
+print(myListAll)
+
+even_number = [2, 4, 6, 8]
+odd_number = [1, 3, 5, 7]
+
+all_number = odd_number + even_number
+
+print(all_number)
